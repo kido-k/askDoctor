aea419cc64bb6db6c9c965eebf955e902977fe3e
kotaro_k <kotaro.k13@gmail.com>
2018-12-07 10:50:40 +0900

    add catch copy

---
+++ components/Atoms/CatchCopy.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="catchCopy">
+    <img
+      class="catchCopyImage"
+      src="../../assets/catch_copy.png"
+      alt="catchCopy">
+  </div>
+</template>
+
+<style scoped>
+.catchCopy {
+  display: inline-block;
+}
+.catchCopyImage {
+  width: 120%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,21 +3,29 @@
     <div class="header">
       <Header/>
     </div>
+    <div class="main">
+      <div class="content1">
+        <CatchCopy/>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 import Header from '~/components/Organisms/TheHeader.vue'
-
+import CatchCopy from '~/components/Atoms/CatchCopy.vue'
 export default {
   components: {
-    Header
+    Header,
+    CatchCopy
   }
 }
 </script>
 
 <style>
 .container {
+  width: 100%;
+  height: 100%;
   margin: 0px;
   padding: 0px;
   text-align: center;
