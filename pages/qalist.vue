4593414110a39a3a7b7a6e3de47c6086c57135bd
kotaro_k <kotaro.k13@gmail.com>
2018-12-12 13:38:04 +0900

    add vuex sample page

---
+++ pages/qalist.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>QA-List</h2>
+    <h3>Count: {{ qalist }}</h3>
+    <button
+      type="button"
+      @click="increment">+</button>
+    <button
+      type="button"
+      @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters({ qalist: 'qalist/getExamples' })
+  },
+  methods: {
+    ...mapActions('qalist', ['increment', 'decrement'])
+  }
+}
+</script>
