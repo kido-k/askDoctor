897374aa8fcc13f069d06772b55a5d4e526de51d
kotaro_k <kotaro.k13@gmail.com>
2018-12-12 13:38:41 +0900

    update consult example

--- store/index.js
+++ store/index.js
@@ -1,42 +1,32 @@
-import Vuex from 'vuex'
+// const state = () => ({
+//   count: 0
+// })
 
-export default () =>
-  new Vuex.Store({
-    state: {
-      items: [],
-      users: {},
-      userItems: {}
-    },
-    getters: {
-      items: state => state.items,
-      users: state => state.users,
-      userItems: state => state.userItems
-    },
-    mutations: {
-      setItems(state, { items }) {
-        state.items = items
-      },
-      setUsers(state, { user }) {
-        state.users[user.id] = user
-      },
-      setUserItems(state, { user, items }) {
-        state.userItems[user.id] = items
-      }
-    },
-    actions: {
-      async fetchItems({ commit }) {
-        const items = await this.$axios.$get(
-          'https://qiita.com/api/v2/items?query=tag:nuxt.js'
-        )
-        commit('setItems', { items })
-      },
-      async fetchUserInfo({ commit }, { id }) {
-        const [user, items] = await Promise.all([
-          this.$axios.$get(`https://qiita.com/api/v2/users/${id}`),
-          this.$axios.$get(`https://qiita.com/api/v2/items?query=user:${id}`)
-        ])
-        commit('setUsers', { user })
-        commit('setUserItems', { user, items })
-      }
-    }
-  })
+// const getters = {
+//   count: state => state.count
+// }
+
+// const mutations = {
+//   increment(state) {
+//     state.count++
+//   },
+//   decrement(state) {
+//     state.count--
+//   }
+// }
+
+// const actions = {
+//   increment({ commit }) {
+//     commit('increment')
+//   },
+//   decrement({ commit }) {
+//     commit('decrement')
+//   }
+// }
+
+// module.exports = {
+//   state,
+//   getters,
+//   mutations,
+//   actions
+// }
